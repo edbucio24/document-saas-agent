5f1cc5a22a03e1de01bdd1920a43a7f44880d592
Edwin <edwin.bucio@wsu.edu>
2026-07-02 15:15:32 -0700

    Providers for Query setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
+import Providers from "@/components/ui/Providers";
  
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <Providers>
           <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+        lang="en"
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+        <body className="min-h-full flex flex-col">{children}</body>
+       </html>
+      </Providers>
     </ClerkProvider>
 
   );
