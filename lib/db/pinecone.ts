f039e80d2e1944ddb6253704648f389f50cd6aa1
Edwin <edwin.bucio@wsu.edu>
2026-07-03 20:36:40 -0700

    pinecone file updated

--- lib/db/pinecone.ts
+++ lib/db/pinecone.ts
@@ -1,5 +1,7 @@
 import {Pinecone} from '@pinecone-database/pinecone'
 import { downloadFromS3 } from './s3-server';
+import {PDFLoader} from "@langchain/community/document_loaders/fs/pdf"
+import { error } from 'console';
 
 let pinecone:Pinecone |null = null
 
@@ -15,6 +17,10 @@ export const getPineconeClient = async()=>{
 export async function loadS3IntoPiencone(fileKey:string){
     console.log('downloading s3 into file sysmtem');
     const file_name = await downloadFromS3(fileKey);
-    
-
+    if(!file_name){
+        throw new Error('could not download')
+    }
+    const loader = new PDFLoader(file_name)
+    const pages = loader.load()
+    return pages;
 }
